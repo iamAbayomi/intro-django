f1aa32009d9b3636cacd1796882c777df28b517b
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-05-20 13:49:38 +0100

    Implemented Vote Method

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import get_object_or_404,render
 from django.shortcuts import Http404
 # Create your views here.from django.http import HttpResponse
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.template import loader
 
-from . models import Question
+from . models import Choice, Question
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -22,4 +23,20 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s. " % question_id)    
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        #Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message' : "You didn't selected a choice. ",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully 
+        # dealing
+        # with POST data. This prevents data from being posted twice if a 
+        # user hits the Back button.
+        return HttpResponse(reverse('polls:results', args=(question.id,)))
