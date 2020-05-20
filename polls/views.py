ff533160e868665f39ed4a480f2c3f3e5795d258
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-05-20 13:32:43 +0100

    Added app_name to polls urls

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
 from django.shortcuts import Http404
 # Create your views here.from django.http import HttpResponse
 from django.http import HttpResponse
@@ -7,10 +7,7 @@ from django.template import loader
 from . models import Question
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")    
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question' : question})
 
 def index(request):
