46a8313512c262e4402c5ebd824205057045591a
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-05-21 12:58:26 +0100

    Change Model.py to reflect current time

--- polls/tests.py
+++ polls/tests.py
@@ -19,5 +19,5 @@ class QuestionModelTests(TestCase):
         """
         time = timezone.now() + datetime.timedelta(days= 30)
         future_question = Question(pub_date=time)
-        self.asserts(future_question.was_published_recently(), False)
+        self.assertIs(future_question.was_published_recently(), False)
 
